0ff87233ade84cf9584477c346b6aac366ae3dd7
shahad7 <shahadneeliyatt786@gmail.com>
2024-03-09 16:24:43 +0530

    logout method + temporary login button

--- ng-client/src/app/components/header/header.component.ts
+++ ng-client/src/app/components/header/header.component.ts
@@ -1,42 +1,48 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
+  constructor(private authService: AuthService) {}
 
-  toggle(){
-    var popup=document.getElementById('popup');
+  logout() {
+    this.authService.logout();
+  }
+
+  toggle() {
+    var popup = document.getElementById('popup');
     popup?.classList.toggle('active');
 
     // If 'copy-id' is present, hide it
-  var copy = document.getElementById('copy-id');
-  if (copy && copy.classList.contains('dis-flex')) {
-    copy.classList.add('dis');
-  }
+    var copy = document.getElementById('copy-id');
+    if (copy && copy.classList.contains('dis-flex')) {
+      copy.classList.add('dis');
+    }
 
-  // If 'join-room' is present, hide it
-  var joinRoom = document.getElementById('join-room');
-  if (joinRoom && joinRoom.classList.contains('dis-flex')) {
-    joinRoom.classList.add('dis');
-  }
+    // If 'join-room' is present, hide it
+    var joinRoom = document.getElementById('join-room');
+    if (joinRoom && joinRoom.classList.contains('dis-flex')) {
+      joinRoom.classList.add('dis');
+    }
 
-    setTimeout(() =>{
+    setTimeout(() => {
       var buttons = document.getElementById('buttons');
       buttons?.classList.remove('dis');
-    },500)
+    }, 500);
   }
 
-  createRoom(){
+  createRoom() {
     var buttons = document.getElementById('buttons');
     buttons?.classList.add('dis');
     var copy = document.getElementById('copy-id');
     copy?.classList.add('dis-flex');
   }
 
-  joinRoom(){
+  joinRoom() {
     var buttons = document.getElementById('buttons');
     buttons?.classList.add('dis');
     var copy = document.getElementById('join-room');
