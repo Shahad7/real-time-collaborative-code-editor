fb985aea0a3185961ce49987980608deb16eee1c
shahad7 <shahadneeliyatt786@gmail.com>
2024-03-16 17:06:35 +0530

    y-textarea initial setup done

--- ng-client/src/app/components/editing-field/editing-field.component.ts
+++ ng-client/src/app/components/editing-field/editing-field.component.ts
@@ -3,18 +3,22 @@ import * as Y from 'yjs';
 import { ViewChild } from '@angular/core';
 import { SocketService } from 'src/app/socket/socket.service';
 import { YArray, YText } from 'yjs/dist/src/internals';
+import { TextAreaBinding } from 'y-textarea';
+import { AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-editing-field',
   templateUrl: './editing-field.component.html',
   styleUrls: ['./editing-field.component.css'],
 })
-export class EditingFieldComponent {
+export class EditingFieldComponent implements AfterViewInit {
   @ViewChild('textarea')
   textarea: any;
 
   //yjs integration
   ydoc = new Y.Doc();
+  ytextarea = this.ydoc.getText('textArea');
+
   constructor(private socketService: SocketService) {
     //defines what to do once the updates from a client arrives
     //have to create a new Uint8Array from the received data
@@ -22,6 +26,19 @@ export class EditingFieldComponent {
     this.socketService.socket.on('receive-updates', (update) => {
       Y.applyUpdate(this.ydoc, new Uint8Array(update));
     });
+
+    //applying event listener to ytextarea :Y.Text
+    this.ytextarea.observe((e) => {
+      console.log(e);
+      this.sendUpdates();
+    });
+  }
+
+  ngAfterViewInit(): void {
+    const areabinding = new TextAreaBinding(
+      this.ytextarea,
+      this.textarea.nativeElement
+    );
   }
 
   sendUpdates() {
