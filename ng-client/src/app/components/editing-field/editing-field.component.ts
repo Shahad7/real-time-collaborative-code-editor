ae6c871a88939bb48b9901d7d63e830be1735832
shahad7 <shahadneeliyatt786@gmail.com>
2024-03-19 15:44:23 +0530

    monaco binding css module error

--- ng-client/src/app/components/editing-field/editing-field.component.ts
+++ ng-client/src/app/components/editing-field/editing-field.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { MonacoEditorModule } from 'ngx-monaco-editor-v2';
+import { MonacoBinding } from 'y-monaco';
+import * as Y from 'yjs';
 
 @Component({
   selector: 'app-editing-field',
@@ -8,4 +11,22 @@ import { Component } from '@angular/core';
 export class EditingFieldComponent {
   editorOptions = { theme: 'vs-light', language: 'javascript' };
   code: string = 'function demo(){ console.log("hey")}';
+  editor: any;
+  binding: any;
+
+  ydoc = new Y.Doc();
+  ytext = this.ydoc.getText('monaco');
+  constructor() {
+    const binding = new MonacoBinding(
+      this.ytext,
+      this.editor.getModel(),
+      new Set([this.editor]),
+      null
+    );
+  }
+
+  onInit(editor: any) {
+    this.editor = editor;
+    console.log(this.editor);
+  }
 }
