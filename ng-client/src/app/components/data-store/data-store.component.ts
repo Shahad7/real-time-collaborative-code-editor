ea7c84b4c19b561ec1bc3b1fb975ac65282cdf73
shahad7 <shahadneeliyatt786@gmail.com>
2024-04-16 17:13:50 +0530

    displays filenames, error n loading..

--- ng-client/src/app/components/data-store/data-store.component.ts
+++ ng-client/src/app/components/data-store/data-store.component.ts
@@ -8,17 +8,29 @@ import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 })
 export class DataStoreComponent implements OnInit {
   roomID: string = '';
+  error: boolean = false;
+  errorMsg: string = '';
+  files: Array<{ filename: string; path: string }> = [];
   constructor(private route: ActivatedRoute, private router: Router) {}
 
   async fetchData() {
     const response = await fetch(
       `http://${window.location.hostname}:3000/room/${this.roomID}`
     );
+
     const data = await response.json();
-    console.log(data);
+    if (response.status == 400) {
+      this.error = true;
+      this.errorMsg = data;
+    } else {
+      data.files.forEach((elt: any) => {
+        this.files.push({ filename: elt.filename, path: elt.path });
+      });
+    }
   }
 
   ngOnInit() {
     this.roomID = this.route.snapshot.paramMap.get('roomID')!;
+    this.fetchData();
   }
 }
